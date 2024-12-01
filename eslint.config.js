037e4366489abcf9c08c291be46ca01fb715cfda
mononoken <ken.maeshima@pm.me>
2024-11-30 17:15:37 -0800

    Config eslint for jsx

--- eslint.config.js
+++ eslint.config.js
@@ -7,10 +7,19 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
   eslintConfigPrettier,
 ];
-
