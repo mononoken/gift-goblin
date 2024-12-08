c950e635ad6e3a7e8d362ff0b4534023b99a211d
mononoken <ken.maeshima@pm.me>
2024-12-08 12:58:34 -0800

    Config vite ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    RubyPlugin(),
-  ],
-})
+  ssr: {
+    noExternal: true,
+  },
+  plugins: [react(), RubyPlugin()],
+});
